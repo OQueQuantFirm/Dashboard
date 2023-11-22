22663279f5941773d7afaf3ed92fe67c895cf3b3
OQue Quant Firm <147313218+OQueQuantFirm@users.noreply.github.com>
2023-11-23 02:16:08 +0800

    Update ai_analysis.py

--- src/market/ai_analysis.py
+++ src/market/ai_analysis.py
@@ -1,35 +1,59 @@
-import asyncio
+import time
 import openai
 import os
 from dotenv import load_dotenv
+import logging
+import yfinance as yf
+from pycoingecko import CoinGeckoAPI
+
+# Setup logging
+logging.basicConfig(level=logging.INFO)
 
 # Load environment variables from .env file
 load_dotenv()
 openai_api_key = os.getenv('OPENAI_API_KEY')
 openai.api_key = openai_api_key
 
-async def fetch_latest_crypto_news():
-    # Placeholder function to fetch latest cryptocurrency news
-    # Replace this with actual code to fetch news from a reliable source
-    return "Bitcoin price surges amid global economic uncertainty. Ethereum follows the trend."
+cg = CoinGeckoAPI()
 
-async def analyze_crypto_news():
+def fetch_latest_crypto_data():
+    """
+    Fetch latest cryptocurrency data using yfinance and CoinGecko.
+    """
+    btc_yfinance = yf.Ticker("BTC-USD")
+    yf_data = btc_yfinance.history(period="1d")
+
+    # Fetch Bitcoin data from CoinGecko
+    cg_data = cg.get_coin_market_chart_by_id(id='bitcoin', vs_currency='usd', days=1)
+
+    return yf_data, cg_data
+
+def analyze_crypto_news(news_cache, sleep_duration=120):
     while True:
-        # Fetch latest cryptocurrency news
-        crypto_news = await fetch_latest_crypto_news()
+        yf_data, cg_data = fetch_latest_crypto_data()
+        yf_news = str(yf_data.iloc[-1].to_dict())  # Convert the last row of yfinance data to a string
+        cg_news = str(cg_data)  # Convert CoinGecko data to a string
 
-        # Use OpenAI to analyze the news
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=f"Provide insights on the following cryptocurrency news: {crypto_news}",
-            max_tokens=60
-        )
+        news_items = [yf_news, cg_news]
 
-        analysis = response.choices[0].text.strip()
-        print(f"Analysis: {analysis}")
+        for news in news_items:
+            news_hash = hash(news)
+            if news_hash not in news_cache:
+                try:
+                    response = openai.Completion.create(
+                        model="text-davinci-003",
+                        prompt=f"Provide insights on the following cryptocurrency data: {news}",
+                        max_tokens=60
+                    )
+                    analysis = response.choices[0].text.strip()
+                    logging.info(f"Analysis: {analysis}")
+                    news_cache.add(news_hash)
+                except Exception as e:
+                    logging.error(f"Error during analysis: {str(e)}")
 
-        # Sleep for 120 seconds
-        await asyncio.sleep(120)
+        time.sleep(sleep_duration)
 
-# Run the async function
-asyncio.run(analyze_crypto_news())
+# Run the function
+if __name__ == "__main__":
+    news_cache = set()
+    analyze_crypto_news(news_cache)
