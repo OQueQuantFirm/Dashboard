a02535fd48203f5c436798e3584a1acdf867b56f
OQue Quant Firm <147313218+OQueQuantFirm@users.noreply.github.com>
2023-11-23 02:00:04 +0800

    Update exchange_with_id.py

--- src/connect/exchange_with_id.py
+++ src/connect/exchange_with_id.py
@@ -1,4 +1,5 @@
 import ccxt
+from src.connect.exchange_instantiate import initialize_exchange
 
 def get_all_exchanges_with_ids():
     """
@@ -13,7 +14,27 @@ def get_all_exchanges_with_ids():
         exchanges_with_ids[exchange.name] = exchange_id
     return exchanges_with_ids
 
-# Fetch and display the exchanges with their IDs
-exchanges_with_ids = get_all_exchanges_with_ids()
-for name, exchange_id in exchanges_with_ids.items():
-    print(f"{name}: {exchange_id}")
+def user_select_exchange(exchanges_with_ids):
+    """
+    Allow the user to select an exchange.
+
+    :param exchanges_with_ids: Dictionary of exchange names and IDs.
+    :return: Selected exchange instance.
+    """
+    print("Available exchanges:")
+    for name, exchange_id in exchanges_with_ids.items():
+        print(f"{name}: {exchange_id}")
+
+    selected_exchange_id = input("Enter the ID of the exchange you prefer: ")
+    if selected_exchange_id in ccxt.exchanges:
+        return initialize_exchange(selected_exchange_id)
+    else:
+        print("Invalid exchange ID.")
+        return None
+
+# Example usage
+if __name__ == "__main__":
+    exchanges_with_ids = get_all_exchanges_with_ids()
+    selected_exchange = user_select_exchange(exchanges_with_ids)
+    if selected_exchange:
+        print(f"Selected exchange: {selected_exchange}")
